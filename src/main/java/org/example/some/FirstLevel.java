a06b5c12e07f87a8bd93b8039b4becca780fff9b
Svitlana Lorman <svlormanua@gmail.com>
2024-05-24 21:45:19 +0300

    changed some buttons

--- src/main/java/org/example/some/FirstLevel.java
+++ src/main/java/org/example/some/FirstLevel.java
@@ -8,6 +8,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,6 +21,7 @@ import java.util.ResourceBundle;
 
 public class FirstLevel implements javafx.fxml.Initializable{
 
+    public AnchorPane anchorPane;
     @FXML
     private Button buttonMenu;
     @FXML
