199adb528570b9941f87c4ba45ae0782abaefca4
Alona Dmytrokhina <dmitrohina1708@gmail.com>
2024-06-06 10:27:47 +0300

    Виправлено меню для кролика і свині, дороблено feeder і well

--- src/main/java/org/example/some/animals/Rabbit.java
+++ src/main/java/org/example/some/animals/Rabbit.java
@@ -127,6 +127,7 @@ public class Rabbit extends AbstractAnimal implements AnimalMeat{
 
     @Override
     public void giveProduct() {
+
         if (FirstLevel.countRabbit >= 1) {
             List<ImageView> productViews = new ArrayList<>();
 
