9954ba124bcc73b903584e803aa1f5f11c738f4f
ShinDasha <d.shyn@ukma.edu.ua>
2024-06-12 23:45:36 +0300

    додано грибок

--- src/main/java/org/example/some/animals/Mushroom.java
+++ src/main/java/org/example/some/animals/Mushroom.java
@@ -4,18 +4,54 @@ import javafx.scene.layout.Pane;
 import org.example.some.Storage;
 import org.example.some.otherGameObjects.Well;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Mushroom extends AbstractAnimal {
-    public Mushroom(int worldStartX, int worldStartY, int worldEndX, int worldEndY, Pane root, Well well, Feeder feeder, Storage storage, String imagePath, String soundFile, String recourseFile) {
-        super(worldStartX, worldStartY, worldEndX, worldEndY, root, well, feeder, storage, imagePath, soundFile, recourseFile);
+    public Mushroom(int worldStartX, int worldStartY, int worldEndX, int worldEndY, Pane root, Well well, Feeder feeder, Storage storage) {
+        super(worldStartX, worldStartY, worldEndX, worldEndY, root, well, feeder, storage,
+                "file:src/main/resources/images/thirdLevel/animals/mushroomR.png",
+                "file:src/main/resources/images/thirdLevel/animals/mushroomL.png",
+                "src/main/resources/sound/dragonfly.mp3",
+                "file:src/main/resources/images/thirdLevel/products/miniMushroom.png");
+        animalView.setFitHeight(90);
+        animalView.setFitWidth(70);
+        giveProduct();
     }
 
     @Override
     public void feed() {
-
+        if(hungerLvl<100) {
+            if(AbstractAnimal.feeder.haveFood()) {
+                hungerLvl += 30;
+            }
+            if (hungerLvl > 100) {
+                hungerLvl = 100;
+            }
+            AbstractAnimal.feeder.getFood();
+        }
     }
 
     @Override
     public void giveProduct() {
 
+    }
+    @Override
+    public void hunger() {
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                if (hungerLvl > 0) {
+                    hungerLvl--;
+                    cost = (int) (1150 * ((double)hungerLvl / 100));
+                } else {
+                    timer.cancel();
+                    timer.purge();
+                }
+            }
+        };
+
+        timer.scheduleAtFixedRate(task, 0, 3000);
     }
 }
